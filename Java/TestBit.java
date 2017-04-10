98169738cae5c6e8f0f0f2c8368b2fc212615570
chaosbynn <t7_chaos@163.com>
2017-04-11 00:04:45 +0800

    create class

---
+++ Java/TestBit.java
@@ -0,0 +1,18 @@
+
+public class TestBit {
+
+	public static void main(String[] args) {
+		
+		System.out.println(0x0107&0xff);
+		
+		System.out.println(0x0107>>8);
+		
+		System.out.println(0x01);
+		
+	}
+	
+}
+
+
+
+
---
+++ Java/TestObject.java
@@ -0,0 +1,14 @@
+
+public class TestObject {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println(String.class.getSuperclass());
+		
+		System.out.println(Object.class.getSuperclass());
+		
+	}
+	
+	
+}
