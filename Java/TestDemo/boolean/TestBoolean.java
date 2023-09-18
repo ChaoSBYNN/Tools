32169062d44193f714e6531cda0e37c2f0f31457
Entropy[但你没有] <ChaoSBYNN@users.noreply.github.com>
2023-09-18 13:49:56 +0800

    Create TestBoolean.java

---
+++ Java/TestDemo/boolean/TestBoolean.java
@@ -0,0 +1,15 @@
+package com.example.testBoolean;
+
+/**
+ * @author Spike_Zhang
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/10/21 10:35
+ */
+public class TestBoolean {
+
+    public static void main(String[] args) {
+        System.out.println(true || false);
+    }
+
+}
