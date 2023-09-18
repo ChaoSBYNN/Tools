15128dffc5fe0d524941857ad76ac7cf2d797037
Entropy[但你没有] <ChaoSBYNN@users.noreply.github.com>
2023-09-18 13:50:27 +0800

    Create TestDoubleCalc.java

---
+++ Java/TestDemo/calc/TestDoubleCalc.java
@@ -0,0 +1,22 @@
+package com.example.testCalc;
+
+/**
+ * @author Spike_Zhang
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/9/16 16:42
+ */
+public class TestDoubleCalc {
+
+    public static void main(String[] args) {
+
+        Double a = 90.96610169491525;
+        Integer b = 91;
+
+        System.out.println(a.intValue());
+        System.out.println(a >= b);
+        System.out.println(a < b);
+
+    }
+
+}
