0110922c52851bfde25ab8b0122192fca4bf598a
Entropy[但你没有] <ChaoSBYNN@users.noreply.github.com>
2023-09-19 11:24:32 +0800

    Create TestHalfAdjust.java

---
+++ Java/TestDemo/halfAdjust/TestHalfAdjust.java
@@ -0,0 +1,19 @@
+package com.example.TestHalfAdjust;
+
+/**
+ * @author Spike_Zhang
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/3/1 10:35
+ */
+public class TestHalfAdjust {
+
+    public static void main(String[] args) {
+        float a = 100f;
+        float b = 85.9f;
+
+        System.out.println((int)((b / a) * 100));
+        System.out.println(((b / a) * 100));
+    }
+
+}
