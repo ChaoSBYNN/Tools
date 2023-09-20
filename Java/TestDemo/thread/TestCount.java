a635ed5f2879dede0bc203831d37615478af7253
Entropy[但你没有] <ChaoSBYNN@users.noreply.github.com>
2023-09-20 10:01:30 +0800

    Create TestCount.java

---
+++ Java/TestDemo/thread/TestCount.java
@@ -0,0 +1,17 @@
+package com.example.testThread;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Spike_Zhang
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/10/31 18:41
+ */
+@Getter
+@Setter
+public class TestCount {
+
+    private Integer count = 0;
+}
