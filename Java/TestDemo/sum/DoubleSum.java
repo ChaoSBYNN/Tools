14d636ddb1a19da48b2c763c0f778fbafecdfc94
Entropy[但你没有] <ChaoSBYNN@users.noreply.github.com>
2023-09-20 10:01:08 +0800

    Create DoubleSum.java

---
+++ Java/TestDemo/sum/DoubleSum.java
@@ -0,0 +1,24 @@
+package com.example.testSum;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author Spike_Zhang
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/8/30 14:21
+ */
+@Getter
+@Setter
+@ToString
+@Builder
+public class DoubleSum {
+
+    private String type;
+
+    private Double value;
+
+}
