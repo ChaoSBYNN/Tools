5be7acf08a63ba761be7f03f4930c0ca42bfffdc
Entropy[但你没有] <ChaoSBYNN@users.noreply.github.com>
2023-09-20 10:00:22 +0800

    Create TestStringConcat.java

---
+++ Java/TestDemo/string/TestStringConcat.java
@@ -0,0 +1,20 @@
+package com.example.testString;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+/**
+ * @author Spike_Zhang
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/1/16 8:59
+ */
+public class TestStringConcat {
+    public static void main(String[] args) {
+        String a = "Hello ";
+        String b = "World!";
+
+        System.out.println(a.concat(b));
+
+    }
+}
