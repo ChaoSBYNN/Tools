b3f1df9f20c09cda8873980c34d1f8e9bcd47697
Entropy[但你没有] <ChaoSBYNN@users.noreply.github.com>
2024-04-04 16:08:21 +0800

    Create RedisListenerConfig.java

---
+++ Java/Redis/RedisListenerConfig.java
@@ -0,0 +1,24 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class RedisListenerConfig {
+
+    @Resource
+    private RedisListener redisListener;
+
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(redisListener, new PatternTopic("LOCK:CONTROL"));
+        return container;
+    }
+
+}
